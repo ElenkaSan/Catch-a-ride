f515227a1139563f487e639b8d84c8a546d803a9
Elenka^_^San <75818489+ElenkaSan@users.noreply.github.com>
2025-04-16 17:46:10 -0400

    fix typo in shema image

--- catch_server/sql/catch-ride-prod.sql
+++ catch_server/sql/catch-ride-prod.sql
@@ -68,7 +68,7 @@ CREATE TABLE vehicle (
  lease_rate DECIMAL(8,2) NOT NULL,
  dealership_id INT NOT NULL,
  booking_status BOOLEAN NOT NULL,
- mage_url TEXT,
+ image_url TEXT,
  FOREIGN KEY (dealership_id) REFERENCES dealership(dealership_id)
  );
 
