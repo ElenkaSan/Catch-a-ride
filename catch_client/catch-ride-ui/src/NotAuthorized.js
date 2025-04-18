381099e1e5af3faeb5bf20a3368a6fa70ee17196
Elenka^_^San <75818489+ElenkaSan@users.noreply.github.com>
2025-04-18 07:45:29 -0400

    added 401 img

--- catch_client/catch-ride-ui/src/NotAuthorized.js
+++ catch_client/catch-ride-ui/src/NotAuthorized.js
@@ -1,11 +1,10 @@
-import React from 'react';
 
 const NotAuthorized = () => {
   return (
-    <div className="container text-center mt-5">
-      <h2 className="text-danger">🚫 Not Authorized</h2>
-      <p>You do not have permission to access this page.</p>
-    </div>
+    <div className="jumbotron text-center" style={{backgroundImage: 'url(./401.jpg)', backgroundSize: 'cover', height: '100vh'}}>
+    <h1 className="text-light display-1">You do not have permission to access this page.</h1>
+    <a className="btn btn-secondary btn-lg p-2" href="/">Go to Home</a>
+  </div>
   );
 };
 
