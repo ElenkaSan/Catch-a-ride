3f170bd5e8a5d990cbf85e47f849b2225110182b
Elenka^_^San <75818489+ElenkaSan@users.noreply.github.com>
2025-04-18 07:46:34 -0400

    text size

--- catch_client/catch-ride-ui/src/NotAuthorized.js
+++ catch_client/catch-ride-ui/src/NotAuthorized.js
@@ -2,7 +2,7 @@
 const NotAuthorized = () => {
   return (
     <div className="jumbotron text-center" style={{backgroundImage: 'url(./401.jpg)', backgroundSize: 'cover', height: '100vh'}}>
-    <h1 className="text-light display-1">You do not have permission to access this page.</h1>
+    <h1 className="text-light display-4 p-2">You do not have permission to access this page.</h1>
     <a className="btn btn-secondary btn-lg p-2" href="/">Go to Home</a>
   </div>
   );
