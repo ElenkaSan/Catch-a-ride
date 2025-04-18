abdc3fbf256b5fc150c8bf73d0b725d93844f449
Elenka^_^San <75818489+ElenkaSan@users.noreply.github.com>
2025-04-17 22:23:05 -0400

    made style navbar homepage"

--- catch_client/catch-ride-ui/src/Home.js
+++ catch_client/catch-ride-ui/src/Home.js
@@ -1,15 +1,19 @@
+import './App.css';
 import Vehicle from "./Vehicle";
 
 function Home(){
     return(
         <div>
-        <header className="text-center p-4 text-info">
-            <h1>Welcome to Catch A Ride application.</h1>
-            <h2>Find cars for lease or rent in your area.</h2>
-        </header>
-       <div className="container">
-            <section className="m-2 p-3">
-                <p className="text-center fw-medium"> 
+        <section className="jarallax min-vh-100 py-lg-3 py-xl-4 py-xxl-5" datajarallax="true" data-speed="0.65">
+           <div className="jarallax-img" style={{backgroundImage: 'url(./home.jpeg)'}}></div>
+           <div className="container-fluid position-relative zindex-2 py-5 my-md-3 my-lg-5 text-light">
+           <div className="row pb-3 pt-4 pt-sm-5">
+            <div className="col-md-9 col-lg-7 col-xl-6 col-xxl-5 offset-lg-1 pt-5">
+            <div className="card home border-0 rounded py-2 py-sm-3 py-md-4" color="secondary">
+            <div className="card-body">
+            <div className="mx-auto pt-2" style={{maxWidth: '535px'}}>
+                    <h2 className="text-center mb-4">Welcome to <br/> Catch A Ride application. </h2>
+                <p className="text-left fw-medium"> 
                 Catch A Ride is the premier application for catching your next ride. Whether you need to lease or rent a car, 
                 Catch A Ride is here to meet all your vehicular supply needs. 
                 Catch A Ride opens up multiple avenues to obtain your next vehicle and offloads your inventory as well.
@@ -17,7 +21,17 @@ function Home(){
                 Here, a user may see listings in their local area and then, when the options allow, rent or lease a vehicle. 
                 Our service offers a fast and efficient portal for any user to obtain vehicles with minimal effort.
                 </p>
-            </section>
+                </div>
+                </div>
+             </div>
+             </div>
+         </div>
+         </div>
+     </section>
+        <header className="text-center p-4 text-info">
+            <h2>Find cars for lease or rent in your area.</h2>
+        </header>
+       <div className="container">
             <section>
                <Vehicle showAvailableOnly={false} />
             </section>
