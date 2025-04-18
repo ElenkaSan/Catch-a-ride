0d5ccbb4d033e6e0480512a6478cc6e54a74d91c
Alexander Chletsos <chletsosa@outlook.com>
2025-04-17 20:59:50 -0400

    Added more data entries to database

--- catch_server/sql/catch-ride-prod.sql
+++ catch_server/sql/catch-ride-prod.sql
@@ -89,13 +89,19 @@ CREATE TABLE booking (
  
  insert into location(location_id, address, city, state, zip_code) values
 	(1, '18 Washington Ave', 'Boston','MA', 20235),
-	(2, '13 Saragota St', 'New York','Ny', 19235);
+	(2, '13 Saragota St', 'New York','Ny', 19235),
+    (3, '123 Main St', 'Trenton', 'NJ', 08753),
+    (4, '42 2nd Ave', 'Philadelphia', 'PA', 19019),
+    (5, '1313 Deadend Dr', 'Bellevue', 'WA', 98004);
 
 
 insert into dealership (dealership_id, `name`, `description`, location_id)
 values
 	(1, 'Autoloco', 'Here for all your vehicular needs', 1),
-	(2, 'Chucklemotor', 'Serving people since 1980', 1);
+	(2, 'Chucklemotor', 'Serving people since 1980', 2),
+    (3, 'Wheelie Good Deals', 'Well steer you in the right direction', 3),
+    (4, 'Miles of Smiles Auto', 'Satisfaction guaranteed', 4),
+    (5, 'The Car-nival', 'Wheel have you rolling over laughing', 5);
     
 insert into app_role (`name`) values
     ('USER'),
@@ -123,7 +129,29 @@ insert into vehicle
 values
 	(1, 'Ford', 'Maverick', '2021', 'Blue', 'Sport', 60.00, 480.00, 1, true),
 	(2, 'Honda', 'Si Base', '2023', 'Silver', 'SE', 50.00, 400.00, 2, true),
-    (3, 'Toyota', 'Camri', '2024', 'Red', 'LE', 50.00, 400.00, 2, false);
+    (3, 'Toyota', 'Camri', '2024', 'Red', 'LE', 50.00, 400.00, 3, false),
+    (4, 'Jeep', 'Recon', '2022', 'Yellow', 'Sport', 50.00, 400.00, 4, false),
+    (5, 'Tesla', 'Model S', '2019', 'Silver', 'P85D', 50.00, 400.00, 5, false),
+    (6, 'Honda', 'Pilot', '2015', 'Grey', 'TrailSport', 50.00, 400.00, 1, false),
+    (7, 'Genesis', 'GV70', '2025', 'Grey', '2.5T', 50.00, 400.00, 2, false),
+    (8, 'Porsche', '911 S/T', '2017', 'Blue', 'Carrera T', 50.00, 400.00, 3, false),
+    (9, 'Toyota', 'Mirai', '2022', 'Blue', 'XLE', 50.00, 400.00, 4, false),
+    (10, 'Ford', 'Edge', '2024', 'White', 'ST-Line', 50.00, 400.00, 5, false),
+    (11, 'Mercedes Benz', 'GLA SUV', '2019', 'Silver', 'GLA 250', 50.00, 400.00, 1, false),
+    (12, 'Ford', 'F-150', '2015', 'Red', 'XL', 50.00, 400.00, 2, false),
+    (13, 'Mercedes Benz', 'E-Class Sedan', '2020', 'Black', 'E 350', 50.00, 400.00, 3, false),
+    (14, 'Toyota', 'Prius', '2024', 'White', ' XLE', 50.00, 400.00, 4, false),
+    (15, 'Lamborghini', 'Urus', '2018', 'Grey', 'Performante', 50.00, 400.00, 5, false),
+    (16, 'Mercedes Benz', 'AMG GT', '2011', 'White', '55', 50.00, 400.00, 1, false),
+    (17, 'Mini', 'Convertible', '2021', 'Green', 'Plus', 50.00, 400.00, 2, false),
+    (18, 'Kia', 'Stinger', '2014', 'Green', 'GT-Line', 50.00, 400.00, 3, false),
+    (19, 'Toyota', 'Corolla', '2016', 'White', 'LE', 50.00, 400.00, 4, false),
+    (20, 'Lexus', 'RX', '2019', 'Orange', 'RX 350', 50.00, 400.00, 5, false),
+    (21, 'BMW', '8', '2025', 'Blue', '840i', 50.00, 400.00, 1, false),
+    (22, 'Jeep', 'Renegade', '2012', 'Orange', 'Latitude', 50.00, 400.00, 2, false),
+    (23, 'Mini', 'Hardtop', '2024', 'White', 'Cooper S', 50.00, 400.00, 3, false),
+    (24, 'Audi', 'A5', '2023', 'White', 'Prestige', 50.00, 400.00, 4, false),
+    (25, 'Hyundai', 'Palisade', '2020', 'Blue', 'SEL Premium', 50.00, 400.00, 5, false);
 
 insert into `booking`
 	(booking_id, vehicle_id, user_id, dealership_location_id, start_date, end_date, booking_type, date_created_at, total_cost)
